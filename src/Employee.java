c1df7ff664fe4ce708bf567c0a2940376f88205a
chantal <chantaluwitonze6@gmail.com>
2025-09-17 10:17:06 +0200

    initial commit

---
+++ src/Employee.java
@@ -0,0 +1,7 @@
+abstract class Employee {
+    public String name;
+    public  long phoneNumber;
+    public String EmailAddress;
+    abstract void calculateSalary();
+    abstract void displayInfo();
+}
